307b3fbcf5a6170027395509a5aa70425dadbd50
Rodrigo Eduardo Hernández Morales <h.rodrigo81@yahoo.com>
2025-10-16 19:25:30 -0600

    primer commit

---
+++ Tarea_3.R
@@ -0,0 +1,4 @@
+library(readxl)
+
+data <- read_excel("C:\\Users\rodri\\OneDrive\\Documentos\\Maestria\\Cuarto_trimestre\\Mineria de datos\\Fp_growth\\violencia_intrafamiliar.csv")
+df <- read.csv("C:\\Users\rodri\\OneDrive\\Documentos\\Maestria\\Cuarto_trimestre\\Mineria de datos\\Fp_growth\\violencia_intrafamiliar.csv")
